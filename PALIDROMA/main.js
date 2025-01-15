9eec66bdc567777bfc08af3df1010b3801385929
ruzica-glavas <ruzi99@hotmail.it>
2025-01-15 16:10:00 +0100

    add: finish the polidroma exercise

--- PALIDROMA/main.js
+++ PALIDROMA/main.js
@@ -1,2 +1,26 @@
-// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+// Chiedere all’utente di inserire una parola e creare una funzione per capire se la parola inserita è palindroma
 
+let scegliUnaParola= prompt ("Digita una parola");
+
+console.log(`La parola scelta é: ${scegliUnaParola}`)
+
+
+function reverseString(parolaDaControllare){
+    // Trasformare la parola in array (con .split). 
+    // Con .reverse cambiare l'ordine degli elementi dell'array
+    // Con .join ritrasformare l'array in una stringa
+
+    return parolaDaControllare.split("").reverse().join("") 
+
+}
+
+    //Risultato della parola al contrario
+console.log(`La parola al contrario é: ${reverseString(scegliUnaParola)}`)
+
+    //Verificare le condizioni di quando una parola é polidroma e di quando non lo é
+
+if(scegliUnaParola==reverseString(scegliUnaParola)){
+    console.log(`La parola ${scegliUnaParola} é Polidroma`)
+}else{
+    console.log(`La parola ${scegliUnaParola} non é Polidroma`)
+}
